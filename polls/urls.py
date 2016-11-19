6321b959963894c1ecfd7051ff3b676cec1c6ba7
jakob <jakob.willforss@hotmail.com>
2016-11-19 15:04:03 +0100

    Tutorial part 3 done. Flexible usage of views in place.

--- polls/urls.py
+++ polls/urls.py
@@ -7,8 +7,11 @@ Here, we seem to have logic to redirect incomers to the poll view
 This is separate from the main URL document for the page
 The admin page is related to page, not poll app
 """
-
+app_name = 'polls'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
 
