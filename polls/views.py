a116f4c73f2a90e6bfeb26c69b5aeb7814e0b659
jakob <jakob.willforss@hotmail.com>
2016-11-19 15:24:17 +0100

    Starting introducing forms for voting and storing results.

--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,7 @@ from django.http import Http404
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
 
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 
@@ -41,7 +41,24 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question {}.".format(question_id))
+
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': 'You didn\'t select a choice',
+        })
+    else:
+        selected_choice.votes += 1
+        # Aha, we are saving directly to database
+        # similarly to when using command-line previously
+        selected_choice.save()
+
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+#    return HttpResponse("You're voting on question {}.".format(question_id))
 
 
 
